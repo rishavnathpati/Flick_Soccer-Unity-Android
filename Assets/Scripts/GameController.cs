9d7eaad0f287d69dfeaecaadc5d54f7b2a165f4f
rishavnathpati <patirishavnath@gmail.com>
2020-06-02 23:34:12 +0530

    Swipe force added to ball

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
@@ -8,6 +6,10 @@ public class GameController : MonoBehaviour
     public GameObject ballPrefab;
     GameObject ballInstance;
     Vector3 mouseStart, mouseEnd;
+    float minSwipeDistance = 15f, zDepth;
+    [SerializeField]
+    float ballForce;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +20,26 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             mouseStart = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseEnd = Input.mousePosition;
+            if (Vector3.Distance(mouseEnd, mouseStart) >= minSwipeDistance)
+            {
+                //throw ball
+                Vector3 hitPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zDepth);
+                hitPos = Camera.main.ScreenToWorldPoint(hitPos);
+                ballInstance.transform.LookAt(hitPos);
+                rb.AddRelativeForce(Vector3.forward * ballForce, ForceMode.Impulse);
+            }
         }
     }
 
     void CreateBall()
     {
-        ballInstance = Instantiate(ballPrefab,ballPrefab.transform.position, Quaternion.identity) as GameObject;
+        ballInstance = Instantiate(ballPrefab, ballPrefab.transform.position, Quaternion.identity) as GameObject;
     }
 }
