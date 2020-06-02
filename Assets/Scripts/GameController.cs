6060afd66459a1989e71d95e99b3b3d1af6d06bc
rishavnathpati <patirishavnath@gmail.com>
2020-06-02 22:35:10 +0530

    Game Controller script added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject ballPrefab;
+    GameObject ballInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateBall()
+    {
+        ballInstance = Instantiate(ballPrefab,ballPrefab.transform.position, Quaternion.identity) as GameObject;
+    }
+}
