862aa686232726cfdb09527d96cd4502ebb62284
rishavnathpati <patirishavnath@gmail.com>
2020-06-03 09:12:06 +0530

    Ball flick movement added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,15 +3,26 @@
 public class GameController : MonoBehaviour
 {
 
-    public GameObject ballPrefab;
-    GameObject ballInstance;
-    Vector3 mouseStart, mouseEnd;
-    float minSwipeDistance = 15f, zDepth;
+    [SerializeField]
+    GameObject ballPrefab;
+
     [SerializeField]
     float ballForce;
-    public Rigidbody rb;
 
-    // Start is called before the first frame update
+    GameObject ballInstance;
+    Vector3 mouseStart;
+    Vector3 mouseEnd;
+
+    float minDragDistance = 15f;
+    float zDepth = 15f;
+
+    void Awake()
+    {
+
+    }
+
+
+    // Use this for initialization
     void Start()
     {
         CreateBall();
@@ -20,26 +31,36 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
             mouseStart = Input.mousePosition;
         }
+
         if (Input.GetMouseButtonUp(0))
         {
             mouseEnd = Input.mousePosition;
-            if (Vector3.Distance(mouseEnd, mouseStart) >= minSwipeDistance)
+
+            if (Vector3.Distance(mouseEnd, mouseStart) > minDragDistance)
             {
                 //throw ball
+
                 Vector3 hitPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zDepth);
+
                 hitPos = Camera.main.ScreenToWorldPoint(hitPos);
+
                 ballInstance.transform.LookAt(hitPos);
-                rb.AddRelativeForce(Vector3.forward * ballForce, ForceMode.Impulse);
+
+                ballInstance.GetComponent<Rigidbody>().AddRelativeForce(ballInstance.transform.forward * ballForce, ForceMode.Impulse);
+
+                Invoke("CreateBall", 2f);
             }
         }
     }
 
     void CreateBall()
     {
+
         ballInstance = Instantiate(ballPrefab, ballPrefab.transform.position, Quaternion.identity) as GameObject;
     }
 }
